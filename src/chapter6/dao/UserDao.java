36d17dbbb2051f245d53d5246d1684ef3a2d902d
ide_hayato <ide.hayato@alhinc.jp>
2025-03-18 17:11:49 +0900

    3/18 実践課題⓵　レビュー用　

--- src/chapter6/dao/UserDao.java
+++ src/chapter6/dao/UserDao.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.exception.NoRowsUpdatedRuntimeException;
 import chapter6.exception.SQLRuntimeException;
@@ -18,213 +20,218 @@ import chapter6.logging.InitApplication;
 
 public class UserDao {
 
+	/**
+	* ロガーインスタンスの生成
+	*/
+	Logger log = Logger.getLogger("twitter");
 
-    /**
-    * ロガーインスタンスの生成
-    */
-    Logger log = Logger.getLogger("twitter");
+	/**
+	* デフォルトコンストラクタ
+	* アプリケーションの初期化を実施する。
+	*/
+	public UserDao() {
+		InitApplication application = InitApplication.getInstance();
+		application.init();
 
-    /**
-    * デフォルトコンストラクタ
-    * アプリケーションの初期化を実施する。
-    */
-    public UserDao(){
-        InitApplication application = InitApplication.getInstance();
-        application.init();
+	}
 
-    }
+	public void insert(Connection connection, User user) {
 
-    public void insert(Connection connection, User user) {
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
 
-	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+		PreparedStatement ps = null;
+		try {
+			StringBuilder sql = new StringBuilder();
+			sql.append("INSERT INTO users ( ");
+			sql.append("    account, ");
+			sql.append("    name, ");
+			sql.append("    email, ");
+			sql.append("    password, ");
+			sql.append("    description, ");
+			sql.append("    created_date, ");
+			sql.append("    updated_date ");
+			sql.append(") VALUES ( ");
+			sql.append("    ?, "); // account
+			sql.append("    ?, "); // name
+			sql.append("    ?, "); // email
+			sql.append("    ?, "); // password
+			sql.append("    ?, "); // description
+			sql.append("    CURRENT_TIMESTAMP, "); // created_date
+			sql.append("    CURRENT_TIMESTAMP "); // updated_date
+			sql.append(")");
 
-        PreparedStatement ps = null;
-        try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("INSERT INTO users ( ");
-            sql.append("    account, ");
-            sql.append("    name, ");
-            sql.append("    email, ");
-            sql.append("    password, ");
-            sql.append("    description, ");
-            sql.append("    created_date, ");
-            sql.append("    updated_date ");
-            sql.append(") VALUES ( ");
-            sql.append("    ?, ");                  // account
-            sql.append("    ?, ");                  // name
-            sql.append("    ?, ");                  // email
-            sql.append("    ?, ");                  // password
-            sql.append("    ?, ");                  // description
-            sql.append("    CURRENT_TIMESTAMP, ");  // created_date
-            sql.append("    CURRENT_TIMESTAMP ");   // updated_date
-            sql.append(")");
+			ps = connection.prepareStatement(sql.toString());
 
-            ps = connection.prepareStatement(sql.toString());
+			ps.setString(1, user.getAccount());
+			ps.setString(2, user.getName());
+			ps.setString(3, user.getEmail());
+			ps.setString(4, user.getPassword());
+			ps.setString(5, user.getDescription());
 
-            ps.setString(1, user.getAccount());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getEmail());
-            ps.setString(4, user.getPassword());
-            ps.setString(5, user.getDescription());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw new SQLRuntimeException(e);
+		} finally {
+			close(ps);
+		}
+	}
 
-            ps.executeUpdate();
-        } catch (SQLException e) {
-		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw new SQLRuntimeException(e);
-        } finally {
-            close(ps);
-        }
-    }
-    public User select(Connection connection, String accountOrEmail, String password) {
+	public User select(Connection connection, String accountOrEmail, String password) {
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		PreparedStatement ps = null;
+		try {
+			String sql = "SELECT * FROM users WHERE (account = ? OR email = ?) AND password = ?";
+
+			ps = connection.prepareStatement(sql);
+
+			ps.setString(1, accountOrEmail);
+			ps.setString(2, accountOrEmail);
+			ps.setString(3, password);
+
+			ResultSet rs = ps.executeQuery();
+
+			List<User> users = toUsers(rs);
+			if (users.isEmpty()) {
+				return null;
+			} else if (2 <= users.size()) {
+				log.log(Level.SEVERE, "ユーザーが重複しています",
+						new IllegalStateException());
+				throw new IllegalStateException("ユーザーが重複しています");
+			} else {
+				return users.get(0);
+			}
+		} catch (SQLException e) {
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw new SQLRuntimeException(e);
+		} finally {
+			close(ps);
+		}
+	}
+
+	private List<User> toUsers(ResultSet rs) throws SQLException {
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		List<User> users = new ArrayList<User>();
+		try {
+			while (rs.next()) {
+				User user = new User();
+				user.setId(rs.getInt("id"));
+				user.setAccount(rs.getString("account"));
+				user.setName(rs.getString("name"));
+				user.setEmail(rs.getString("email"));
+				user.setPassword(rs.getString("password"));
+				user.setDescription(rs.getString("description"));
+				user.setCreatedDate(rs.getTimestamp("created_date"));
+				user.setUpdatedDate(rs.getTimestamp("updated_date"));
+
+				users.add(user);
+			}
+			return users;
+		} finally {
+			close(rs);
+		}
+	}
+
+	public User select(Connection connection, int id) {//ユーザ設定変更
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		PreparedStatement ps = null;
+		try {
+			String sql = "SELECT * FROM users WHERE id = ?";
+
+			ps = connection.prepareStatement(sql);
+
+			ps.setInt(1, id);
+
+			ResultSet rs = ps.executeQuery();
+
+			List<User> users = toUsers(rs);
+			if (users.isEmpty()) {
+				return null;
+			} else if (2 <= users.size()) {
+				log.log(Level.SEVERE, "ユーザーが重複しています", new IllegalStateException());
+				throw new IllegalStateException("ユーザーが重複しています");
+			} else {
+				return users.get(0);
+			}
+		} catch (SQLException e) {
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw new SQLRuntimeException(e);
+		} finally {
+			close(ps);
+		}
+	}
+
+	public void update(Connection connection, User user) {
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		PreparedStatement ps = null;
+		try {
+			StringBuilder sql = new StringBuilder();
 
 
-  	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-          " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-
-          PreparedStatement ps = null;
-          try {
-              String sql = "SELECT * FROM users WHERE (account = ? OR email = ?) AND password = ?";
-
-              ps = connection.prepareStatement(sql);
-
-              ps.setString(1, accountOrEmail);
-              ps.setString(2, accountOrEmail);
-              ps.setString(3, password);
-
-              ResultSet rs = ps.executeQuery();
-
-              List<User> users = toUsers(rs);
-              if (users.isEmpty()) {
-                  return null;
-              } else if (2 <= users.size()) {
-  		    log.log(Level.SEVERE,"ユーザーが重複しています",
-                  new IllegalStateException());
-                  throw new IllegalStateException("ユーザーが重複しています");
-              } else {
-                  return users.get(0);
-              }
-          } catch (SQLException e) {
-  		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-              throw new SQLRuntimeException(e);
-          } finally {
-              close(ps);
-          }
-      }
-
-      private List<User> toUsers(ResultSet rs) throws SQLException {
-
-  	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-          " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-
-          List<User> users = new ArrayList<User>();
-          try {
-              while (rs.next()) {
-                  User user = new User();
-                  user.setId(rs.getInt("id"));
-                  user.setAccount(rs.getString("account"));
-                  user.setName(rs.getString("name"));
-                  user.setEmail(rs.getString("email"));
-                  user.setPassword(rs.getString("password"));
-                  user.setDescription(rs.getString("description"));
-                  user.setCreatedDate(rs.getTimestamp("created_date"));
-                  user.setUpdatedDate(rs.getTimestamp("updated_date"));
-
-                  users.add(user);
-              }
-              return users;
-          } finally {
-              close(rs);
-          }
-      }
-
-      public User select(Connection connection, int id) {//ユーザ設定変更
-
-
-    	    log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-    	    " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-
-    	    PreparedStatement ps = null;
-    	    try {
-    	        String sql = "SELECT * FROM users WHERE id = ?";
-
-    	        ps = connection.prepareStatement(sql);
-
-    	        ps.setInt(1, id);
-
-    	        ResultSet rs = ps.executeQuery();
-
-    	        List<User> users = toUsers(rs);
-    	        if (users.isEmpty()) {
-    	            return null;
-    	        } else if (2 <= users.size()) {
-    	    		log.log(Level.SEVERE, "ユーザーが重複しています", new IllegalStateException());
-    	            throw new IllegalStateException("ユーザーが重複しています");
-    	        } else {
-    	            return users.get(0);
-    	        }
-    	    } catch (SQLException e) {
-    		  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-    	        throw new SQLRuntimeException(e);
-    	    } finally {
-    	        close(ps);
-    	    }
-    	}
-
-      public void update(Connection connection, User user) {
-
-    	    log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-    	    " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-
-    	    PreparedStatement ps = null;
-    	    try {
-	    		StringBuilder sql = new StringBuilder();
+			sql.append("UPDATE users SET ");
+			sql.append("    account = ?, ");
+			sql.append("    name = ?, ");
+			sql.append("    email = ?, ");
 //	    		実践課題①クエリの分岐
-    	    	if(user.getPassword().equals("noChange")) {
-        	        sql.append("UPDATE users SET ");
-        	        sql.append("    account = ?, ");
-        	        sql.append("    name = ?, ");
-        	        sql.append("    email = ?, ");
-        	        sql.append("    description = ?, ");
-        	        sql.append("    updated_date = CURRENT_TIMESTAMP ");
-        	        sql.append("WHERE id = ?");
+			if (!StringUtils.isEmpty(user.getPassword())) {
+				sql.append("    password = ?, ");
+			}
+			sql.append("    description = ?, ");
+			sql.append("    updated_date = CURRENT_TIMESTAMP ");
+			sql.append("WHERE id = ?");
 
-        	        ps = connection.prepareStatement(sql.toString());
+			ps = connection.prepareStatement(sql.toString());
 
-        	        ps.setString(1, user.getAccount());
-        	        ps.setString(2, user.getName());
-        	        ps.setString(3, user.getEmail());
-        	        ps.setString(4, user.getDescription());
-        	        ps.setInt(5, user.getId());
-    	    	} else {
-    	    		sql.append("UPDATE users SET ");
-    	    		sql.append("    account = ?, ");
-    	    		sql.append("    name = ?, ");
-    	    		sql.append("    email = ?, ");
-    	    		sql.append("    password = ?, ");
-    	    		sql.append("    description = ?, ");
-    	    		sql.append("    updated_date = CURRENT_TIMESTAMP ");
-    	    		sql.append("WHERE id = ?");
+			ps.setString(1, user.getAccount());
+			ps.setString(2, user.getName());
+			ps.setString(3, user.getEmail());
+//	    		実践課題①クエリの分岐
+			if (!StringUtils.isEmpty(user.getPassword())) {
+				ps.setString(4, user.getPassword());
+				ps.setString(5, user.getDescription());
+				ps.setInt(6, user.getId());
+			} else {
+				ps.setString(4, user.getDescription());
+				ps.setInt(5, user.getId());
+			}
 
-    	    		ps = connection.prepareStatement(sql.toString());
-
-    	    		ps.setString(1, user.getAccount());
-    	    		ps.setString(2, user.getName());
-    	    		ps.setString(3, user.getEmail());
-    	    		ps.setString(4, user.getPassword());
-    	    		ps.setString(5, user.getDescription());
-    	    		ps.setInt(6, user.getId());
-    	    	}
-    	        int count = ps.executeUpdate();
-    	        if (count == 0) {
-    	    		log.log(Level.SEVERE,"更新対象のレコードが存在しません", new NoRowsUpdatedRuntimeException());
-    	            throw new NoRowsUpdatedRuntimeException();
-    	        }
-    	    } catch (SQLException e) {
-    		  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-    	        throw new SQLRuntimeException(e);
-    	    } finally {
-    	        close(ps);
-    	    }
-    	}
-  }
+			int count = ps.executeUpdate();
+			if (count == 0) {
+				log.log(Level.SEVERE, "更新対象のレコードが存在しません", new NoRowsUpdatedRuntimeException());
+				throw new NoRowsUpdatedRuntimeException();
+			}
+		} catch (SQLException e) {
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw new SQLRuntimeException(e);
+		} finally {
+			close(ps);
+		}
+	}
+}
--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -7,139 +7,154 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
 import chapter6.utils.CipherUtil;
 
-
 public class UserService {
 
+	/**
+	* ロガーインスタンスの生成
+	*/
+	Logger log = Logger.getLogger("twitter");
 
-    /**
-    * ロガーインスタンスの生成
-    */
-    Logger log = Logger.getLogger("twitter");
+	/**
+	* デフォルトコンストラクタ
+	* アプリケーションの初期化を実施する。
+	*/
+	public UserService() {
+		InitApplication application = InitApplication.getInstance();
+		application.init();
 
-    /**
-    * デフォルトコンストラクタ
-    * アプリケーションの初期化を実施する。
-    */
-    public UserService() {
-        InitApplication application = InitApplication.getInstance();
-        application.init();
+	}
 
-    }
+	public void insert(User user) {
 
-    public void insert(User user) {
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
 
+		Connection connection = null;
+		try {
+			// パスワード暗号化
+			String encPassword = CipherUtil.encrypt(user.getPassword());
+			user.setPassword(encPassword);
 
-	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+			connection = getConnection();
+			new UserDao().insert(connection, user);
+			commit(connection);
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
 
-        Connection connection = null;
-        try {
-            // パスワード暗号化
-            String encPassword = CipherUtil.encrypt(user.getPassword());
-            user.setPassword(encPassword);
+	public User select(String accountOrEmail, String password) {
 
-            connection = getConnection();
-            new UserDao().insert(connection, user);
-            commit(connection);
-        } catch (RuntimeException e) {
-            rollback(connection);
-		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } catch (Error e) {
-            rollback(connection);
-		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } finally {
-            close(connection);
-        }
-    }
-    public User select(String accountOrEmail, String password) {
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
 
-  	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-          " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+		Connection connection = null;
+		try {
+			// パスワード暗号化
+			String encPassword = CipherUtil.encrypt(password);
 
-          Connection connection = null;
-          try {
-              // パスワード暗号化
-              String encPassword = CipherUtil.encrypt(password);
+			connection = getConnection();
+			User user = new UserDao().select(connection, accountOrEmail, encPassword);
+			commit(connection);
 
-              connection = getConnection();
-              User user = new UserDao().select(connection, accountOrEmail, encPassword);
-              commit(connection);
+			return user;
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
 
-              return user;
-          } catch (RuntimeException e) {
-              rollback(connection);
-  		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-              throw e;
-          } catch (Error e) {
-              rollback(connection);
-  		log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-              throw e;
-          } finally {
-              close(connection);
-          }
-      }
+	public User select(int userId) {//ユーザ設定変更
 
-    public User select(int userId) {//ユーザ設定変更
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
 
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			User user = new UserDao().select(connection, userId);
+			commit(connection);
 
-        log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+			return user;
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
 
-        Connection connection = null;
-        try {
-            connection = getConnection();
-            User user = new UserDao().select(connection, userId);
-            commit(connection);
+	public void update(User user) {
 
-            return user;
-        } catch (RuntimeException e) {
-            rollback(connection);
-    	  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } catch (Error e) {
-            rollback(connection);
-    	  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } finally {
-            close(connection);
-        }
-    }
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
 
-    public void update(User user) {
+		Connection connection = null;
+		try {
+			// パスワード暗号化
+			//        	実践課題その①暗号化のエスケープ
+			if (!StringUtils.isEmpty(user.getPassword())) {
+				String encPassword = CipherUtil.encrypt(user.getPassword());
+				user.setPassword(encPassword);
+			}
 
-        log.info(new Object(){}.getClass().getEnclosingClass().getName() + 
-        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
-
-        Connection connection = null;
-        try {
-            // パスワード暗号化
-//        	実践課題その①暗号化のエスケープ
-        	if (!user.getPassword().equals("noChange") ) {
-        		String encPassword = CipherUtil.encrypt(user.getPassword());
-        		user.setPassword(encPassword);
-        	}
-
-            connection = getConnection();
-            new UserDao().update(connection, user);
-            commit(connection);
-        } catch (RuntimeException e) {
-            rollback(connection);
-    	  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } catch (Error e) {
-            rollback(connection);
-    	  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
-            throw e;
-        } finally {
-            close(connection);
-        }
-    }
+			connection = getConnection();
+			new UserDao().update(connection, user);
+			commit(connection);
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
 
 }
